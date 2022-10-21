22368c9c9a804fad2f38555a6c5a3efd5b5754b8
Philip Basnight <philipbasnight@gmail.com>
2022-10-21 11:06:34 -0400

    mongoose installed

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.send({ msg: 'This route is being hit' })
+})
+
 app.listen(PORT, () => {
   console.log(`Express server listening on port: ${PORT}`)
 })
