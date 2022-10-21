95f24e4f1390372fdb355435c2df62e242b23aeb
Philip Basnight <philipbasnight@gmail.com>
2022-10-21 17:17:11 -0400

    reviews displaying on front end

--- albumreviews-frontend/src/App.js
+++ albumreviews-frontend/src/App.js
@@ -3,13 +3,23 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 function App() {
+  const [reviews, updateProducts] = useState([])
+
   useEffect(() => {
-    let response = axios.get('')
-    console.log(response)
+    const apiCall = async () => {
+      let response = await axios.get('http://localhost:3001/reviews')
+      updateProducts(response.data)
+    }
+    apiCall()
   }, [])
   return (
     <div className="App">
       <h1>All Album Reviews Here</h1>
+      {reviews.map((review) => (
+        <div>
+          <h1>{review.name}</h1>
+        </div>
+      ))}
     </div>
   )
 }
