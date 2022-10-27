3011c0f25eb0c8414b39a95efbc0e9392e7e815c
Philip Basnight <philipbasnight@gmail.com>
2022-10-27 13:54:26 -0400

    styling and code clean up

--- albumreviews-frontend/src/pages/AlbumDetails.js
+++ albumreviews-frontend/src/pages/AlbumDetails.js
@@ -2,26 +2,6 @@ import { React, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 
-// const AlbumDetails = (props) => {
-//   const [album, setAlbum] = useState('')
-//   let { id } == useParams()
-
-//   useEffect(() => {
-//     let selectedAlbum = props.db.find((album) => album.id === parseInt(id))
-//     setAlbum(selectedAlbum)
-//   }, [props.db, _id])
-
-//   return album ? (
-//     <div>
-//       <div>
-//         <img src={album.art} />
-//         <h1>{album.name}</h1>
-//       </div>
-//     </div>
-//   ) : null
-// }
-// export default AlbumDetails
-
 const AlbumDetails = () => {
   return (
     <div className="AlbumDetails">
