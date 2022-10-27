3ee8b0614857195511b3b007f9fa4b592439054b
Philip Basnight <philipbasnight@gmail.com>
2022-10-27 17:32:13 -0400

    express middleware added

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express()
 //middleware
 app.use(express.json())
 app.use(cors())
+app.use(express.static(`${__dirname}/client/build`))
 
 app.get('/', (req, res) => {
   res.send({ msg: 'This route is being hit' })
