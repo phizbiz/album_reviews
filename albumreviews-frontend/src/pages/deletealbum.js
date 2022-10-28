28ded8ff71d8d66a38c2dab4ab1b6fbfe1cddc82
Philip Basnight <philipbasnight@gmail.com>
2022-10-28 10:48:40 -0400

    camelcase adjustments

--- albumreviews-frontend/src/pages/deletealbum.js
+++ albumreviews-frontend/src/pages/deletealbum.js
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import link from 'react-router-dom'
 import Albums from './albums'
-// import Reviews from './reviews'
 
 const DeleteAlbum = () => {
   const [albums, updateAlbums] = useState([])
