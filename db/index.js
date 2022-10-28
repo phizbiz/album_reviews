699d64c2f31e93218b217fc698e3d3d67ca6f948
Philip Basnight <philipbasnight@gmail.com>
2022-10-28 10:28:45 -0400

    added

--- db/index.js
+++ db/index.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose')
 // below could be placed in server.js
 require('dotenv').config()
 
-let dbUrl =
-  process.env.NODE_ENV === 'production'
-    ? process.env.MONGODB_URI
-    : 'mongodb://127.0.0.1:27017/albumReviewDatabase'
-
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
